6e6a02cd0947a23b42fa95427718d083832924fe
Semih-Sinani <semihsinaniofficial@gmail.com>
2023-08-15 01:14:30 +0200

    Completed

--- index.py
+++ index.py
@@ -15,3 +15,13 @@ for i in range(pwd_length):
   pwd += ''.join(secrets.choice(alphabet))
 
 print(pwd)
+
+while True:
+  pwd = ''
+  for i in range(pwd_length):
+    pwd += ''.join(secrets.choice(alphabet))
+
+  if (any(char in special_chars for char in pwd) and 
+      sum(char in digits for char in pwd)>=2):
+          break
+print(pwd)
